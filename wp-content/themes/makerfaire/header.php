cbb38615285cdb8a19790c31d5c4b0f353aacb3a
Bill Olson <bolson@makermedia.com>
2015-02-18 14:06:54 -0800

    Remarketing pixel

--- wp-content/themes/makerfaire/header.php
+++ wp-content/themes/makerfaire/header.php
@@ -31,6 +31,26 @@
 
 	<?php wp_head(); ?>
 
+	<!-- Remarketing pixel -->
+
+	<script type="text/javascript">
+		adroll_adv_id = "QZ72KCGOPBGLLLPAE3SDSI";
+		adroll_pix_id = "RGZKRB7CHJF5RBMNCUJREU";
+		(function () {
+		var oldonload = window.onload;
+		window.onload = function(){
+		   __adroll_loaded=true;
+		   var scr = document.createElement("script");
+		   var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+		   scr.setAttribute('async', 'true');
+		   scr.type = "text/javascript";
+		   scr.src = host + "/j/roundtrip.js";
+		   ((document.getElementsByTagName('head') || [null])[0] ||
+		    document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+		   if(oldonload){oldonload()}};
+		}());
+	</script>
+
 	<!-- TypeKit -->
 	<link rel="stylesheet" href="http://use.typekit.com/c/4690c1/museo-slab:n8:n9:n1:n3,bebas-neue:n4,proxima-nova:n4:i4:n7:i7,museo-slab:n5.QL3:F:2,QL5:F:2,QL7:F:2,SKB:F:2,TGd:F:2,W0V:F:2,W0W:F:2,W0Y:F:2,W0Z:F:2,WH7:F:2/d?3bb2a6e53c9684ffdc9a98f6135b2a62e9fd3f37bbbb30d58844c72ca542eb12d9fc18cda0192bd960a04b65e2f2facc738d907514640137ac74942ecfe54dd35844bc349bb4c1279a7aaf8651616db7b59a075388454f5f4a07fb5c0b8f09dcccc3d70f9605ca7a1dbf9b12b3c351656254cd3fc59e92f2e542459e636860be01542f5c784cda4fe2fc310798ac7c1670eeda393aa990e8b58d73431e6bae280cf620ce09d0a49a9554ea7f25339dd274cf69ee61d55e93d9cb159fd2848203940e4eb67ad0455b5b574d1a27fec0ae65">
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>

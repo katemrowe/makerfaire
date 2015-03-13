73e8a23def5916fddbfaae2b121bd581a63b9870
David Beauchamp <dbeauchamp@makermedia.com>
2015-03-13 12:15:21 -0700

    Changing the footer link from contact us to help.

--- wp-content/themes/makerfaire/footer.php
+++ wp-content/themes/makerfaire/footer.php
@@ -89,7 +89,7 @@
 
 							<li><a href="http://makerfaire.com/makerfairehistory/">About</a></li>
 							<li><a href="http://makezine.com/tag/maker-faire/">Blog</a></li>
-							<li><a href="http://makerfaire.com/contact/">Contact Us</a></li>
+							<li><a href="https://help.makermedia.com" target="_blank">Help</a></li>
 							<li><a href="http://makerfaire.com/newsletter/">Newsletter</a></li>
 							<li><a href="http://makermedia.com/privacy/">Privacy Policy</a></li>
 							<li><a href="http://makerfaire.com/sponsors/">Sponsors</a></li>

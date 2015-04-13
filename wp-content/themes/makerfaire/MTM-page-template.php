84936cd800c613381d5b13940aefee4306cade19
Rich Haynie <rhaynie@slashdotmedia.com>
2015-04-12 21:01:24 -0600

    Image fixes.

--- wp-content/themes/makerfaire/MTM-page-template.php
+++ wp-content/themes/makerfaire/MTM-page-template.php
@@ -85,7 +85,7 @@
 								<div class="mask">
 									<div class="slideset">
 			      <div class="slide">
-   		                <a href="/bay-area-15/maker/entry/<?php echo $randEntry['id']; ?>"><span class="maker-slider-btn">Learn More About This Maker</span><img src="<?php echo $randEntry['22']; ?>" height="442" width="935" alt=""></a>
+   		                <a href="/bay-area-15/maker/entry/<?php echo $randEntry['id']; ?>"><span class="maker-slider-btn">Learn More About This Maker</span><img src="<?php echo legacy_get_resized_remote_image_url($randEntry['22'],935,442); ?>" height="442" width="935" alt=""></a>
 				<a href="/bay-area-15/maker/entry/<?php echo $entries[$i]['id']; ?>"><div class="text-holder">
 				   <strong class="title">Featured Maker Story</strong>
 				   <p><mark><?php echo $randEntry['151']; ?>: </mark><?php echo $randEntry['16']; ?></p>
@@ -93,7 +93,7 @@
 			      </div>
                               <?php for ($i = 0; $i < count($entries); $i++) { if ($i == $randEntryKey) { continue; }  ?>
                                 <div class="slide">
-   		                  <a href="/bay-area-15/maker/entry/<?php echo $entries[$i]['id']; ?>"><span class="maker-slider-btn">Learn More About This Maker</span><img src="<?php echo $entries[$i]['22']; ?>" height="442" width="935" alt=""></a>
+   		                  <a href="/bay-area-15/maker/entry/<?php echo $entries[$i]['id']; ?>"><span class="maker-slider-btn">Learn More About This Maker</span><img src="<?php echo legacy_get_resized_remote_image_url($entries[$i]['22'],935,442); ?>" height="442" width="935" alt=""></a>
 				  <a href="/bay-area-15/maker/entry/<?php echo $entries[$i]['id']; ?>"><div class="text-holder">
 				     <strong class="title">Featured Maker Story</strong>
 				     <p><mark><?php echo $entries[$i]['151']; ?>: </mark><?php echo $entries[$i]['16']; ?></p>

3e55c1aaaa929a354079dca8de01d4fbbb4bc38a
Rich Haynie <rhaynie@slashdotmedia.com>
2015-04-27 08:05:41 -0600

    Error Logging for venue api issue.

--- wp-content/themes/makerfaire/api/v2/venue/index.php
+++ wp-content/themes/makerfaire/api/v2/venue/index.php
@@ -59,7 +59,7 @@ if ( $type == 'venue' ) {
 	FROM `wp_mf_api_venue`
 			WHERE `wp_mf_api_venue`.faire = '$faire' ");
  	$result = $mysqli->query ( $select_query );
-	
+ 	error_log('API_VENUE_QUERY_RESULT='.print_r($result,true)):
 	// Loop through the posts
 	while ( $row = $result->fetch_row () ) {
 		

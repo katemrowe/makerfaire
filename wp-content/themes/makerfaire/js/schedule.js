cb00ca9d3a35a49e3cd46fc0339c39eb6383c16f
Alicia Williams <alicia.williams130@gmail.com>
2015-05-11 16:47:46 -0600

    unmark all others as active when the hash is set in the url

--- wp-content/themes/makerfaire/js/schedule.js
+++ wp-content/themes/makerfaire/js/schedule.js
@@ -8,6 +8,7 @@ window.onload=function (e) {
    if (jQuery( "#scheduleTab" ).length ) { //do the schedule tabs exist?S
         var hash = window.location.hash;
         if(hash!=''){ //was a hash set?
+            jQuery('.dropdown li').each(function(){ jQuery(this).removeClass('active')});
             jQuery('.dropdown a[href="'+hash+'"]').tab('show') // Select tab by name
             var dispText = jQuery('.dropdown a[href="'+hash+'"]').text(); // Select tab by name
             //set dropdown text

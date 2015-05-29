bd911a313a0fa1c529183858e7fe805ff2fb210a
Alicia Williams <alicia.williams130@gmail.com>
2015-05-29 15:15:35 -0600

    changes for MF-167, MF-171 and MF-172 Adding sql to daily cron job to create wp_mf_api_entity and wp_mf_maker

--- wp-content/themes/makerfaire/classes/cronJob.php
+++ wp-content/themes/makerfaire/classes/cronJob.php
@@ -3,232 +3,121 @@
  * this script will hold all the cronjobs for makerfaire
  */
 
+/*
 //for testing
-/*define( 'BLOCK_LOAD', true );
+define( 'BLOCK_LOAD', true );
 require_once( '../../../../wp-config.php' );
 require_once( '../../../../wp-includes/wp-db.php' );
 $wpdb = new wpdb( DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
-*/
- $dataArray = array( 'Contact'       =>array(
-                            
-                            'Bio'           => '',
-                            'Email'         => '98',
-                            'First Name'    => '96.3',
-                            'Last Name'     => '96.6',
-                            'OnsitePhone'   => '99',
-                            'Photo'         => '',
-                            'TWITTER'       => '',
-                        ),
-                        'Presenter'     =>array(
-                            
-                            'Bio'           => '234',
-                            'Email'         => '161',
-                            'First Name'    => '160.3',
-                            'Last Name'     => '160.6',
-                            'OnsitePhone'   => '265',
-                            'Photo'         => '217',
-                            'TWITTER'       => '201',
-                        ),                        
-                        'Presenter2'    =>array(
-                            
-                            'Bio'           => '',
-                            'Email'         => '162',
-                            'First Name'    => '158.3',
-                            'Last Name'     => '158.6',
-                            'OnsitePhone'   => '266',
-                            'Photo'         => '224',
-                            'TWITTER'       => '208',
-                        ),
-                        'Presenter3'    =>array(
-                            
-                            'Bio'           => '',
-                            'Email'         => '167',
-                            'First Name'    => '155.3',
-                            'Last Name'     => '155.6',
-                            'OnsitePhone'   => '267',
-                            'Photo'         => '223',
-                            'TWITTER'       => '207',
-                        ),
-                        'Presenter4'    =>array(
-                                 
-                            'Bio'           => '',
-                            'Email'         => '166',
-                            'First Name'    => '156.3',
-                            'Last Name'     => '156.6',
-                            'OnsitePhone'   => '268',
-                            'Photo'         => '222',
-                            'TWITTER'       => '206',
-                        ),
-                        'Presenter5'    =>array(
-                            
-                            'Bio'           => '',
-                            'Email'         => '165',
-                            'First Name'    => '157.3',
-                            'Last Name'     => '157.6',
-                            'OnsitePhone'   => '268',
-                            'Photo'         => '220',
-                            'TWITTER'       => '205',
-                        ),
-                        'Presenter6'    =>array(
-                            
-                            'Bio'           => '',
-                            'Email'         => '164',
-                            'First Name'    => '159.3',
-                            'Last Name'     => '159.6',
-                            'OnsitePhone'   => '270',
-                            'Photo'         => '221',
-                            'TWITTER'       => '204',
-                        ),
-                        'Presenter7'    =>array(
-                            
-                            'Bio'           => '',
-                            'Email'         => '163',
-                            'First Name'    => '154.3',
-                            'Last Name'     => '154.6',
-                            'OnsitePhone'   => '271',
-                            'Photo'         => '219',
-                            'TWITTER'       => '203',
-                        ),
-                        'Group'         =>array(
-                            
-                            'Bio'           => '110',
-                            'Email'         => '',
-                            'First Name'    => '109',
-                            'Last Name'     => '',
-                            'OnsitePhone'   => '',
-                            'Photo'         => '111',
-                            'TWITTER'       => '',
-                        )
-        );
-//dailyCronFunction(); //for testing
+dailyCronFunction(); //for testing*/
+
 add_action('dailyCronHook', 'dailyCronFunction');
 
 function dailyCronFunction(){    
    build_wp_mf_maker();
-   // build_wp_mf_maker_summary();
+   build_wp_mf_api_entity();
     
+}
+function build_wp_mf_api_entity(){
+    global $wpdb;
+    $sql = "REPLACE INTO `wp_mf_api_entity`
+            (`ID`,
+            `project_title`,
+            `project_description`,
+            `project_url`,
+            `category_id`,
+            `child_id_ref`,
+            `thumb_image_url`,
+            `large_image_url`)
+            SELECT 
+        `wp_rg_lead_detail`.`lead_id` AS `lead_id`,
+        trim(GROUP_CONCAT(DISTINCT IF((FORMAT(`wp_rg_lead_detail`.`field_number`,
+                    2) = 151),
+                `wp_rg_lead_detail`.`value`,
+                NULL)
+            SEPARATOR ',')) AS `Title`,COALESCE(
+        trim(GROUP_CONCAT(DISTINCT IF((FORMAT(`wp_rg_lead_detail`.`field_number`,
+                    2) = 16),
+                `l`.`value`,
+                NULL)
+            SEPARATOR ',')),trim(GROUP_CONCAT(DISTINCT IF((FORMAT(`wp_rg_lead_detail`.`field_number`,
+                    2) = 16),
+                `wp_rg_lead_detail`.`value`,
+                NULL)
+            SEPARATOR ','))) AS `Description`,
+       trim( GROUP_CONCAT(DISTINCT IF((FORMAT(`wp_rg_lead_detail`.`field_number`,
+                    2) = 27),
+                `wp_rg_lead_detail`.`value`,
+                NULL)
+            SEPARATOR ',')) AS `URL`,
+         trim(GROUP_CONCAT(DISTINCT IF((FORMAT(`wp_rg_lead_detail`.`field_number`,
+                    2) BETWEEN 146.9999 AND 147.9999),
+                    SUBSTRING_INDEX(SUBSTRING_INDEX(`wp_rg_lead_detail`.`value`, ':', 2), ':', -1),
+               NULL)
+            SEPARATOR ',')) AS `Categories`,
+		trim(GROUP_CONCAT(distinct maker_id
+            SEPARATOR ',')) AS `maker_ids`,
+		trim(GROUP_CONCAT(distinct IF((FORMAT(`wp_rg_lead_detail`.`field_number`,
+                    2) = 22),
+                `wp_rg_lead_detail`.`value`,
+                NULL)
+            SEPARATOR ',')) AS `Photo`,
+        trim(GROUP_CONCAT(DISTINCT IF((FORMAT(`wp_rg_lead_detail`.`field_number`,
+                    2) = 22),
+                `wp_rg_lead_detail`.`value`,
+                NULL)
+            SEPARATOR ',')) AS `ThumbPhoto`
+    FROM
+        (`wp_rg_lead_detail`
+        left outer JOIN `wp_rg_lead_detail_long` `l` ON ((`wp_rg_lead_detail`.`id` = `l`.`lead_detail_id`)))
+        JOIN `wp_rg_lead` `b` ON ((`wp_rg_lead_detail`.`lead_id` = `b`.`id`))
+        JOIN `wp_mf_maker` `c` ON (`b`.`id` = `c`.lead_id) and not isnull(`c`.`First Name`)
+    GROUP BY `wp_rg_lead_detail`.`lead_id`;";
+     $wpdb->get_results($sql);
 }
 function build_wp_mf_maker(){
-    
-    global $wpdb; global $dataArray;
-        
-    $tableData = array();
-    //pull data for table
-    $nameID = 0;
-    foreach($dataArray as $type=>$lead_detail){   
-        $sql = '';
-        $sql .= "select  `b`.`id` AS `lead_id`,
-                    group_concat(if((format(`wp_rg_lead_detail`.`field_number`,2) = 76),`wp_rg_lead_detail`.`value`,NULL) separator ',') AS `SpecialRequest`,
-                    group_concat(if((format(`wp_rg_lead_detail`.`field_number`,2) = 151),`wp_rg_lead_detail`.`value`,NULL) separator ',') AS `PresentationTitle`,
-                    group_concat(if((format(`wp_rg_lead_detail`.`field_number`,2) = 1),`wp_rg_lead_detail`.`value`,NULL) separator ',') AS `PresentationType`,
-                    group_concat(if((format(`wp_rg_lead_detail`.`field_number`,2) between 301.9999 and 302.9999),`wp_rg_lead_detail`.`value`,NULL) separator ',') AS `Location`,
-                    max(if((`wp_rg_lead_detail`.`field_number` = 303),`wp_rg_lead_detail`.`value`,NULL)) AS `Status`,
-                    `b`.`form_id` AS `form_id`,
-                    concat(`wp_rg_lead_detail`.`lead_id`,'-".$nameID."') AS `maker_id`, ";
-        $nameID++;            
-        foreach($lead_detail as $field_value=>$field_number){
-            
-            if($field_number==''){
-                $sql.= "'' as '".$field_value."', ";
-            }else{
-                $sql .= "group_concat(if((format(`wp_rg_lead_detail`.`field_number`,2) = ".$field_number."),`wp_rg_lead_detail`.`value`,NULL) separator ',') AS `".$field_value."`,";
-            }
-        }
-        $sql .= "'".$type."' as 'Name'                
-                from `wp_rg_lead_detail` 
-                    join `wp_rg_lead` `b` 
-                        on`wp_rg_lead_detail`.`lead_id` = `b`.`id`
-                group by `wp_rg_lead_detail`.`lead_id` ";
-        
-        foreach( $wpdb->get_results($sql, ARRAY_A ) as $key=>$row) {
-            // each column in your row will be accessible like this
-            $tableData[] = $row;            
-        }
-
-    }
-        
-        
-    //drop current table
-    $sql = "DROP TABLE IF EXISTS `wp_mf_maker`;";
+    global $wpdb;
+    //first empty existing table
+    $sql = "Truncate Table wp_mf_maker;";
     $wpdb->get_results($sql);
-    
-    //create table
-    $sql = "CREATE TABLE `wp_mf_maker` (
-              `ID` int(11) NOT NULL,
-              `lead_id` int(11) unsigned NOT NULL DEFAULT '0',
-              `First Name` varchar(341) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-              `Last Name` varchar(341) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-              `Bio` varchar(341) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-              `Email` varchar(341) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-              `OnsitePhone` varchar(341) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-              `TWITTER` varchar(341) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-              `SpecialRequest` varchar(341) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-              `PresentationTitle` varchar(341) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-              `PresentationType` varchar(341) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-              `Name` varchar(10) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
-              `Location` varchar(341) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-              `Status` varchar(1024) DEFAULT NULL,
-              `form_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-              `maker_id` varchar(12) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
-              `Photo` varchar(1024) DEFAULT NULL
-            ) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
-    $wpdb->get_results($sql);
-    
-    $sql = "ALTER TABLE `wp_mf_maker` ADD PRIMARY KEY (`ID`)";
-    $wpdb->get_results($sql); 
-    
-    $sql = "ALTER TABLE `wp_mf_maker` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;";    
-    $wpdb->get_results($sql);   
-    //print_r($tableData);
-    $labels = array_keys($tableData[0]);
-    $labels = array('lead_id', 'First Name', 'Last Name', 'Bio', 'Email', 
-                    'OnsitePhone', 'TWITTER', 'SpecialRequest', 
-                    'PresentationTitle', 'PresentationType', 'Name', 'Location', 
-                    'Status', 'form_id', 'maker_id', 'Photo');
-    //insert data rows
-    $sqlInsert = "insert into wp_mf_maker (";
-    $i = 0;
-    $numLabels = count($labels);
-    foreach($labels as $label){
-        $sqlInsert .= '"'.$label.'"';
-        if(++$i === $numLabels) {
-            //
-        }else{
-            $sqlInsert .= ",";
-        }
-        
-    }
-    $sqlInsert .=") values (";
-    $sqlInsert = "INSERT INTO `wp_mf_maker`(`lead_id`, `First Name`, `Last Name`, `Bio`, `Email`, `OnsitePhone`, `TWITTER`, `SpecialRequest`, `PresentationTitle`, `PresentationType`, `Name`, `Location`, `Status`, `form_id`, `maker_id`, `Photo`) VALUES (";
-    $rows = 0;
-    $numRows = count($tableData);
-    foreach($tableData as $value){                  
-        $sql ='';
-        $i = 0;
-        $numItems = count($labels);
-        foreach($labels as $label){
-            if($label == 'lead_id' || $label == 'form_id'){ //numeric
-                $sql.= $value[$label];                
-            }else{
-                $sql .= '"'.$value[$label].'"';
-            }
-            
-            if(++$i === $numItems) {
-               //
-            }else{
-                $sql .= ",";
-            }
-        }
-        echo $sqlInsert.$sql.')<br/><br/>';
-        $wpdb->get_results($sqlInsert.$sql.')');         
-    }
-    //echo 'sql='.$sqlInsert.'<br/><br/>';
-        
+    //now populate with new data
+    $sql = "INSERT INTO `wp_mf_maker`
+    (`lead_id`,
+    `First Name`,
+    `Last Name`,
+    `Bio`,
+    `Email`,
+    `OnsitePhone`,
+    `TWITTER`,
+    `SpecialRequest`,
+    `PresentationTitle`,
+    `PresentationType`,
+    `Name`,
+    `Location`,
+    `Status`,
+    `Photo`,
+    `form_id`,
+    `maker_id`
+    )
+    SELECT `wp_gravityforms_maker_view`.`lead_id`,
+        `wp_gravityforms_maker_view`.`First Name`,
+        `wp_gravityforms_maker_view`.`Last Name`,
+        `wp_gravityforms_maker_view`.`Bio`,
+        `wp_gravityforms_maker_view`.`Email`,
+        `wp_gravityforms_maker_view`.`OnsitePhone`,
+        `wp_gravityforms_maker_view`.`TWITTER`,
+        `wp_gravityforms_maker_view`.`SpecialRequest`,
+        `wp_gravityforms_maker_view`.`PresentationTitle`,
+        `wp_gravityforms_maker_view`.`PresentationType`,
+        `wp_gravityforms_maker_view`.`Name`,
+        `wp_gravityforms_maker_view`.`Location`,
+        `wp_gravityforms_maker_view`.`Status`,
+        `wp_gravityforms_maker_view`.`Photo`,
+        `wp_gravityforms_maker_view`.`form_id`,
+        `wp_gravityforms_maker_view`.`maker_id`
+    FROM `wp_gravityforms_maker_view`;";
+     $wpdb->get_results($sql);
     
 }
 
-function build_wp_mf_maker_summary(){
-    
-    
-}
 

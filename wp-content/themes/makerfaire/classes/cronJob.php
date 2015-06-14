cb1c1bf0e06e7dcb51bb6cfdef4c4eeaf5606718
Alicia Williams <alicia.williams130@gmail.com>
2015-06-14 15:01:43 -0600

    splitting the two table builds into separate functions to keep the job from timing out

--- wp-content/themes/makerfaire/classes/cronJob.php
+++ wp-content/themes/makerfaire/classes/cronJob.php
@@ -12,11 +12,13 @@ $wpdb = new wpdb( DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
 build_wp_mf_maker(); //for testing*/
 
 add_action('dailyCronHook', 'dailyCronFunction');
+add_action('dailyCronHook', 'dailyCronAPIFunction');
 
 function dailyCronFunction(){    
-   build_wp_mf_maker();
-   build_wp_mf_api_entity();
-    
+   build_wp_mf_maker();    
+}
+function dailyCronAPIFunction(){    
+   build_wp_mf_api_entity();   
 }
 function build_wp_mf_api_entity(){
     global $wpdb;

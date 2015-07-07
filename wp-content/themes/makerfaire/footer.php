820239292b9594244040203d8c5501c0fdd3d23d
Alicia Williams <alicia.williams130@gmail.com>
2015-07-07 13:14:45 -0600

    Changes for MF-345 Place Tracking pixel on site for World Maker Faire Promo

--- wp-content/themes/makerfaire/footer.php
+++ wp-content/themes/makerfaire/footer.php
@@ -408,6 +408,17 @@ if ( is_page( '459885' ) ) {
 <!-- End pop up modal for school page -->
 
 <?php wp_footer(); ?>
+<script type="text/javascript">
+  (function() {
+    window._pa = window._pa || {};
+    // _pa.orderId = ""; // OPTIONAL: attach unique conversion identifier to conversions
+    // _pa.revenue = ""; // OPTIONAL: attach dynamic purchase values to conversions
+    // _pa.productId = ""; // OPTIONAL: Include product ID for use with dynamic ads
+    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+    pa.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + "//tag.marinsm.com/serve/558d98991eb60ba078000001.js";
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(pa, s);
+  })();
+</script>
 
 </body>
 </html>

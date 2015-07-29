3058a8b28d25ef27aa32237be558c7094ff6335c
Alicia Williams <alicia.williams130@gmail.com>
2015-07-29 10:45:40 -0600

    MF-422 fix to return the taxonomy label instead of the taxonomy code in the export

--- wp-content/themes/makerfaire/classes/gf-helper.php
+++ wp-content/themes/makerfaire/classes/gf-helper.php
@@ -266,3 +266,25 @@ function setTaxName($value, $field, $lead, $form){
 	}
     
 }
+
+add_filter( 'gform_export_field_value', 'set_export_values', 10, 4 );
+function set_export_values( $value, $form_id, $field_id, $lead ) {
+    
+    if($field_id==320){
+        $form = GFAPI::get_form( $form_id );
+    
+        foreach( $form['fields'] as $field ) {
+            if ( $field->id == $field_id) {
+                if( in_array( $field->type, array('checkbox', 'select', 'radio') ) ){
+                    $value = RGFormsModel::get_lead_field_value( $lead, $field );
+                    return GFCommon::get_lead_field_display( $field, $value, $lead["currency"], true );
+                }else{
+                        return $value;
+                }
+
+            }
+
+        }
+    }
+    return $value;
+}

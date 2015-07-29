3610cd3e85b3c256182968a2d7926601069d55e6
Alicia Williams <alicia.williams130@gmail.com>
2015-07-29 10:41:59 -0600

    disable 10x10 when selling at maker faire or promoting a product or service is selected

--- wp-content/themes/makerfaire/js/gravityforms.js
+++ wp-content/themes/makerfaire/js/gravityforms.js
@@ -2,13 +2,12 @@ jQuery( document ).ready(function() {
 
 	 jQuery(".checkbox_makerfaire_plans input").on("click", function() {
 	    var value = jQuery(this).attr("value");
-	 	if (value=='Selling at Maker Faire [Commercial Maker]' || value=='Promoting a product or service [Commercial Maker]')
+	 	if (value==='Selling at Maker Faire [Commercial Maker]' || value==='Promoting a product or service [Commercial Maker]')
 			{
 	 			var checked1=jQuery('input[value="Selling at Maker Faire [Commercial Maker]"]').attr("checked");
 				var checked2=jQuery('input[value="Promoting a product or service [Commercial Maker]"]').attr("checked");
-				var disablesizes = (checked1 == 'checked' || checked2=='checked');
-				if (disablesizes)
-					{
+				var disablesizes = (checked1 === 'checked' || checked2==='checked');
+				if (disablesizes){
 				jQuery('input[value="10\' x 10\'"]').prop("checked",false);
                                 jQuery('input[value="10\' x 20\'"]').prop("checked",false);                                
 				jQuery('input[value="Other"]').prop("checked",false);

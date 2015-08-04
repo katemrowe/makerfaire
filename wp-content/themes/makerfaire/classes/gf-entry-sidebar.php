13767814d3c71d51287fa37a8a94f667a76d01b8
Alicia Williams <alicia.williams130@gmail.com>
2015-08-04 11:03:23 -0600

    fixed issue from merge

--- wp-content/themes/makerfaire/classes/gf-entry-sidebar.php
+++ wp-content/themes/makerfaire/classes/gf-entry-sidebar.php
@@ -102,6 +102,7 @@ function mf_sidebar_entry_schedule($form_id, $lead) {
                                 FROM `wp_mf_schedule` 
                                 join wp_mf_faire on wp_mf_schedule.faire = wp_mf_faire.faire
                                 where entry_id=$entry_id")  or trigger_error($mysqli->error);
+       
 	if ($result)
 	{
 	while($row = $result->fetch_row())
@@ -109,15 +110,14 @@ function mf_sidebar_entry_schedule($form_id, $lead) {
 		$start_dt = strtotime( $row[4]);
 		$end_dt = strtotime($row[5]);
 		$schedule_entry_id = $row[0];
-
-		echo ('<input type="checkbox" value="'.$schedule_entry_id.'" style="margin: 3px;float:left" name="delete_entry_id[]"></input><span>'.date("l, n/j/y, g:i A",$start_dt).'(PT) to '.date("l, n/j/y, g:i A",$end_dt).'(PT)</span><br />');
-
+		
                 //set time zone for faire
                 $dateTime = new DateTime(); 
                 $dateTime->setTimeZone(new DateTimeZone($row[7])); 
                 $timeZone = $dateTime->format('T'); 
-		echo ('<input type="checkbox" value="'.$schedule_entry_id.'" style="margin: 3px;float:left" name="delete_entry_id[]"></input>'
-                        . '<span style="line-height: 1.3em;">'.date("l, n/j/y, g:i A",$start_dt).'('.$timeZone.') to '.date("l, n/j/y, g:i A",$end_dt).'('.$timeZone.')</span><br />');		
+		echo ('<div style="padding:5px 0;    border-bottom: thin solid lightgray;">'
+                        . ' <input type="checkbox" value="'.$schedule_entry_id.'" style="margin: 3px;float:left;8px 5px;" name="delete_entry_id[]"></input>'
+                        . '<span style="line-height: 1.3em;">'.date("l, n/j/y, g:i A",$start_dt).'('.$timeZone.') to '.date("l, n/j/y, g:i A",$end_dt).'('.$timeZone.')</span><br /></div>');		
 	}
         echo '<br/>';
 	$entry_delete_button = '<input type="submit" name="delete_entry_schedule[]" value="Delete Selected" class="button"

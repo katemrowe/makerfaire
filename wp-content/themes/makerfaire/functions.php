eee6770b12710bf1001528d5f2483383b501d304
Alicia Williams <alicia.williams130@gmail.com>
2015-09-04 12:35:09 -0600

    B&N fix for missing JS.  All custom JS that is used in gravity forms needs to be hooked into gravity forms with the gform_enqueue_scripts hook

--- wp-content/themes/makerfaire/functions.php
+++ wp-content/themes/makerfaire/functions.php
@@ -117,7 +117,7 @@ function make_enqueue_jquery() {
 	wp_enqueue_script( 'make-bootstrapdialog',  get_stylesheet_directory_uri() . '/js/bootstrap-dialog.min.js', array( 'jquery' ), null );
 	//wp_enqueue_script( 'make-login-dialog',  get_stylesheet_directory_uri() . '/js/login.js', array( 'jquery' ), null );
 	//wp_enqueue_script( 'make-confirmation-dialog',  get_stylesheet_directory_uri() . '/js/confirmation.js', array( 'jquery' ), null );
-	wp_enqueue_script( 'make-gravityforms',  get_stylesheet_directory_uri() . '/js/gravityforms.js', array( 'jquery' ), null );
+	
 	wp_enqueue_script( 'bootgrid',  get_stylesheet_directory_uri() . '/plugins/grid/jquery.bootgrid.min.js', array( 'jquery' ), null );
 	wp_enqueue_script( 'thickbox',null, array( 'jquery' ), null );
          wp_enqueue_script( 'faireSchedule',  get_stylesheet_directory_uri() . '/js/schedule.js', array( 'jquery' ), null ); 
@@ -127,6 +127,11 @@ function make_enqueue_jquery() {
 	
 }
 add_action( 'wp_enqueue_scripts', 'make_enqueue_jquery' );
+add_action( 'gform_enqueue_scripts', 'enqueue_custom_script', 10, 2 );
+
+function enqueue_custom_script( $form, $is_ajax ) {
+    wp_enqueue_script( 'make-gravityforms',  get_stylesheet_directory_uri() . '/js/gravityforms.js', array( 'jquery' ), null );
+}
 
 function load_admin_scripts() {
 

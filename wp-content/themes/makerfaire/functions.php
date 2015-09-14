9828b780475009dfcad12335b17dee9a8f02a528
Alicia Williams <alicia.williams130@gmail.com>
2015-09-14 12:02:38 -0600

    fix for B&N store select questions. when back button was used the store select options were not being saved

--- wp-content/themes/makerfaire/functions.php
+++ wp-content/themes/makerfaire/functions.php
@@ -1288,6 +1288,25 @@ function populate_html( $form ) {
     
     return($form);
 }   
+
+//for the Barnes and Noble forms the store preference is set by JS. 
+//If the back button is selected we need to populate these fields 
+add_filter( 'gform_pre_render_43', 'BN_storeSelect' );
+function BN_storeSelect( $form ) {    
+    if(isset($_POST["input_341"]) || isset($_POST["input_342"]) || isset($_POST["input_343"])){        
+        //add selected values to form
+        foreach ( $form['fields'] as &$field ) {
+            if($field->id==341 || $field->id==342 || $field->id==343){
+                $choices = array();
+                $storeSel = rgpost("input_".$field->id);                
+                $choices[] = array( 'text' => $storeSel, 'value' => $storeSel);
+                $field->choices = $choices;                
+            }
+        }
+    }
+    return($form);
+}   
+
 //when form is submitted, find the initial formid based on entry id and add the fields to that entry
 add_action( 'gform_after_submission', 'GSP_after_submission', 10, 2 );
 function GSP_after_submission($entry, $form ){    

b03f6d26ca3858ff2739591e26fcee7ba322951e
Rich Haynie <rhaynie@slashdotmedia.com>
2015-09-14 18:32:27 -0600

    Fix to image resizing.

--- wp-content/themes/makerfaire/classes/legacy-helper.php
+++ wp-content/themes/makerfaire/classes/legacy-helper.php
@@ -61,6 +61,20 @@ function legacy_get_resized_remote_image_url( $url, $width, $height, $escape = t
 	return ( $escape ) ? esc_url( $thumburl ) : $thumburl;
 	endif;
 }
+function legacy_get_fit_remote_image_url( $url, $width, $height, $escape = true ) {
+	if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'photon' ) ) :
+	$width = (int) $width;
+	$height = (int) $height;
+
+	// Photon doesn't support redirects, so help it out by doing http://foobar.wordpress.com/files/ to http://foobar.files.wordpress.com/
+	if ( function_exists( 'new_file_urls' ) )
+		$url = new_file_urls( $url );
+
+	$thumburl = jetpack_photon_url( $url, array( 'fit' => array( $width, $height ) ) );
+
+	return ( $escape ) ? esc_url( $thumburl ) : $thumburl;
+	endif;
+}
 /**
  * Returns the body from a requested url
  *

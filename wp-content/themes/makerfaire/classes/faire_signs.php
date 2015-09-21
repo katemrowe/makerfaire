8b7a0c8831409f28e727dce8cd3c1d392ff59bee
Alicia Williams <alicia.williams130@gmail.com>
2015-09-21 13:37:48 -0600

    Revert "changes for faire export"
    This reverts commit 31e320868214b8b33504a078cb26ca507d0e113f.

--- wp-content/themes/makerfaire/classes/faire_signs.php
+++ wp-content/themes/makerfaire/classes/faire_signs.php
@@ -12,42 +12,17 @@ $entries         = GFAPI::get_entries( $form_id, $search_criteria, $sorting, $pa
 
 ?>
 <h2>Print All Signs for NY15</h2>
-<div style="width:75%; float:left">
-    To create all signs for the faire, click the button to the right.<br/>
-    Individual signs can be created by clicking on the entry ID below.  <br/>
-    If a sign has previously been created, it will be listed next to the entry ID with the date of creation.
-
-</div>
-<input class="button button-large button-primary" style="text-align:center;float:left" value="Create all <?php echo $total_count;?> signs" id="processButton"   onClick="printSigns()"/>
-<br/><br/>
-<div style="clear:both"></div>
-    <div width="100%">
-        <ul>
-
-    <?php
+<input class="button button-large button-primary" style="text-align:center" value="Create all <?php echo $total_count;?> signs" id="processButton"   onClick="printSigns()"/><br/>
+<?php
 //var_dump($entries);
-
-foreach($entries as $entry){    
+foreach($entries as $entry){
     $entry_id = $entry['id'];
     ?>
-        <li style="float: left;width: 25%;">
-            <?php echo $entry_id.': ';?>
-            <a class="fairsign" target="_blank" id="<?php echo $entry_id;?>" href="/wp-content/themes/makerfaire/fpdi/makersigns.php?eid=<?php echo $entry_id;?>">Create</a>
-        
-    
-        <?php
-    $filename = TEMPLATEPATH.'/signs/NY15/'.$entry_id.'.pdf';
-    if (file_exists($filename)) {
-      echo ' (<a target="_blank" href="/'.$filename.'">'.date ("m/d/Y H:i:s", filemtime($filename)).'</a>)';
-    }
-    ?>
-    </li>
-    
-<?php
+    <a class="fairsign" target="_blank" id="<?php echo $entry_id;?>" href="/wp-content/themes/makerfaire/fpdi/makersigns.php?eid=<?php echo $entry_id;?>"><?php echo $entry_id;?></a><br/>  
+             <?php
 }
 ?>
-        </ul>
-</div>
+
 <script>
     
         jQuery(document).ready(function(){
@@ -55,7 +30,7 @@ foreach($entries as $entry){
          });
          
          function printSigns(){
-            jQuery('#processButton').val("Creating PDF's. . . ");
+             jQuery('#processButton').val("Creating PDF's. . . ");
             jQuery("a.fairsign").each(function(){                
             
                 jQuery(this).html('Creating');

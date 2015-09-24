0df4783f5f4f4e8105212dcb14190fa94983c96e
Alicia Williams <alicia.williams130@gmail.com>
2015-09-24 14:56:38 -0400

    fix for location duplicates

--- wp-content/themes/makerfaire/signage-list.php
+++ wp-content/themes/makerfaire/signage-list.php
@@ -51,11 +51,12 @@ function get_schedule_list( $location, $short_description = false, $day_set = ''
             where   schedule.faire          = '".$faire."' 
                     AND schedule.entry_id   = entity.lead_id 
                     AND entity.status       = 'Accepted' 
+                    and wp_mf_schedule.location_id=wp_mf_location.ID
                     and location.entry_id   = schedule.entry_id
                     and subarea.id          = location.subarea_id
                     and area.id             = subarea.area_id"                    
                     .($day_set!=''?" and DAYNAME(`schedule`.`start_dt`)='".ucfirst($day_set)."'":'');    
-        $sql .= " group by Day, 'Start Time', 'End Time', subarea.nicename, area.area,'Exhibit', Presenters";           
+            
         if($orderBy=='time'){
             $sql .= " order by schedule.start_dt ASC, schedule.end_dt ASC, subarea.nicename ASC, 'Exhibit' ASC";            
         }else{

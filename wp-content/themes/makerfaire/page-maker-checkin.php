2ed5b0f463f67e15ec58508b21cdca6fe10d14f7
Alicia Williams <alicia.williams130@gmail.com>
2015-09-25 13:48:37 -0400

    changes to maker checkin page

--- wp-content/themes/makerfaire/page-maker-checkin.php
+++ wp-content/themes/makerfaire/page-maker-checkin.php
@@ -3,6 +3,10 @@
 //determine the parent page slug
  $par_post = get_post($post->post_parent);
  $slug = $par_post->post_name;
+ 
+ $entryID = (isset($_GET['entryID'])?$_GET['entryID']:'');
+ 
+ 
 ?>
 
 <div class="clear"></div>
@@ -22,56 +26,78 @@
 					<?php /*<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1> */ ?>
 
 					<?php /*<p class="meta top">By <?php the_author_posts_link(); ?>, <?php the_time('Y/m/d \@ g:i a') ?></p> */ ?>
+                                        <?php
+                                        //form submitted? update database
+                                        if(isset($_POST['submit'])){
+                                           $entryID    = (isset($_POST['entryID'])   && $_POST['entryID']   != ''     ? $_POST['entryID']     :0);
+                                           $latitude   = (isset($_POST['latitude'])  && $_POST['latitude']  != ''    ? $_POST['latitude']    :0);
+                                           $longitude  = (isset($_POST['longitude']) && $_POST['longitude'] !=''  ? $_POST['longitude']   :0);
+                                           $geotext    = (isset($_POST['geotext'])     ? $_POST['geotext']     :'');
+                                           $comments   = (isset($_POST['comments'])    ? $_POST['comments']    :'');
+                                           //update the database with submitted info
+                                           $sql = "INSERT INTO `maker_checkin`(`entry_id`, `latitude`, `longitude`, `geotext`, `comments`) VALUES "
+                                                   . "($entryID,$latitude,$longitude,'$geotext','$comments')";
 
-					<?php the_content(); ?>
+                                          $wpdb->get_results($sql);
+                                          echo '<h2>Thank you for your submission</h2>';
+                                        }else{
+                                            ?>
+                                            <?php the_content(); ?>
 
-					<div class="clear"></div>
-                                        <p>Click the button to get your coordinates.</p>
+                                            <div class="clear"></div>
+                                            <p>Hello <?php echo $entryID; ?>,<br/><br/>
+                                            Please wait while we get your location.    
+                                           <br/><br/>
 
-                                        <button onclick="getLocation()">Try It</button>
+                                            <form id="geolocation" method="POST" action="">
+                                                <input id="entryID"   name="entryID"   type="hidden" value ="<?php echo $entryID; ?>" />
+                                                Latitude: 
+                                                <input id="latitude"  name="latitude"  type="text" />
+                                                Longitude: 
+                                                <input id="longitude" name="longitude" type="text"  /><br/>
+                                                Text from GeoLocation: <br/>
+                                                <input style="width:500px;height:200px" type="textarea" rows="4" cols="50" id="geotext" name="geotext" /><br/>
+                                                Comments: <br/>
+                                                <input style="width:500px;height:200px"  type="textarea" rows="4" cols="50"id="comments" name="comments" /><br/>
+                                                <input type="submit" name="submit" value="Submit your location." />
+                                            </form>
 
-                                        <p id="demo"></p>
+                                            <script>
+                                            var x = document.getElementById("demo");
+                                            jQuery(document).ready(function(){
+                                                getLocation();
+                                            });
 
-                                        <script>
-                                        var x = document.getElementById("demo");
-
-                                        function getLocation() {
-                                            if (navigator.geolocation) {
-                                                navigator.geolocation.getCurrentPosition(showPosition);
-                                            } else { 
-                                                x.innerHTML = "Geolocation is not supported by this browser.";
+                                            function getLocation() {
+                                                if (navigator.geolocation) {
+                                                    navigator.geolocation.getCurrentPosition(showPosition);
+                                                } else { 
+                                                    x.innerHTML = "Geolocation is not supported by this browser.";
+                                                }
                                             }
+
+                                            function showPosition(position) {
+                                               jQuery('#latitude').val(position.coords.latitude);
+                                               jQuery('#longitude').val(position.coords.longitude);
+                                               
+                                               jQuery('#geotext').val();
+                                               var output = '';
+                                               output = 'Returned Data: <br/>' + 
+                                                        'Latitude: '  +         position.coords.latitude  + "\n"+
+                                                        'Longitude: ' +         position.coords.longitude + "\n"+
+                                                        'Accuracy: '  +         position.coords.accuracy  + '\n'+
+                                                        'Altitude: '  +         position.coords.altitude  + '\n'+
+                                                        'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n'+
+                                                        'Heading as degrees clockwise from North: ' +   position.coords.heading	+ '\n'+
+                                                        'Speed (meters): '  +  position.coords.speed + '\n'+
+                                                        'Timestamp: '       +  position.timestamp;
+                                               jQuery('#geotext').val(output);
+
+                                            }
+                                            </script>
+					<?php 
                                         }
-
-                                        function showPosition(position) {
-                                            x.innerHTML = "Latitude: " + position.coords.latitude + 
-                                            "<br>Longitude: " + position.coords.longitude;	
-                                        }
-                                        </script>
-					<?php /*
-					<div class="row">
-
-						<div class="postmeta">
-
-							<div class="span-thumb img-thumbnail">
-
-								<?php echo get_avatar( get_the_author_meta('user_email'), 72); ?>
-
-							</div>
-
-							<div class="span-well well">
-
-								<p>Posted by <?php the_author_posts_link(); ?> | <a href="<?php the_permalink(); ?>"><?php the_time('l F jS, Y g:i A'); ?></a></p>
-								<p>Categories: <?php the_category(', '); ?> | <?php comments_popup_link(); ?> <?php edit_post_link('Fix me...', ' | '); ?></p>
-
-							</div>
-
-						</div>
-
-						<div class="clear"></div>
-
-					</div>
-					*/ ?>
+                                       ?>
 
 				</article>
 
@@ -92,7 +118,7 @@
 
 		</div><!--Content-->
 
-		<?php get_sidebar(); ?>
+		<?php //get_sidebar(); ?>
 
 	</div>
 

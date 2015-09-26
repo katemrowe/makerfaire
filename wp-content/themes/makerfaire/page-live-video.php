fe7e58a9126fff9bb1c2ae3c6a822388894e7e97
Bill Olson <bolson@makermedia.com>
2015-09-25 21:50:50 -0400

    Live page updates

--- wp-content/themes/makerfaire/page-live-video.php
+++ wp-content/themes/makerfaire/page-live-video.php
@@ -9,22 +9,22 @@
 <div class="clear"></div>
 <div class="container live-page">
 	<div class="row" style="margin-bottom:0px;padding-bottom:0px;">
-		<div class="col-xs-7">
+		<div class="col-md-7">
 			<div class="row">
 				<div class="col-xs-12">
 					<h1><?php echo get_the_title(); ?></h1>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-xs-12">
-					<h2 style="font-size:14px;color:#333;margin-top:0px;">Saturday 10am-8pm PT | Sunday 10am-6pm PT</h2>
+				<div class="col-md-12">
+					<h2 style="font-size:14px;color:#333;margin-top:0px;">Saturday 10am-6pm ET | Sunday 10am-6pm ET</h2>
 				</div>
 			</div>
 		</div>
-		<div class="col-xs-2">
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/robot.png" width="auto" alt="makey robot" />
+		<div class="col-md-2">
+			<img class="makeybot" src="<?php echo get_stylesheet_directory_uri(); ?>/images/robot.png" width="auto" alt="makey robot" />
 		</div>
-		<div class="col-xs-3 social">
+		<div class="col-md-3 social">
 			<div class="social-foot-col">
 				<div class="social-profile-icons">
 					<a class="sprite-facebook-32" href="//www.facebook.com/sharer/sharer.php?u=http://makerfaire.com/live" title="Facebook" target="_blank">
@@ -52,7 +52,7 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-xs-12">
+		<div class="col-md-12">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<article <?php post_class(); ?>>
 				<?php the_content(); ?>
@@ -76,7 +76,7 @@
 						<div class="row">
 							<div class="col-xs-12 col-sm-8">
 								<div class="title">
-									<h1>Bay Area Maker Faire Sponsors</h1>
+									<h1>Maker Faire Sponsors</h1>
 								</div>
 							</div>
 							<div class="col-xs-12 col-sm-4">

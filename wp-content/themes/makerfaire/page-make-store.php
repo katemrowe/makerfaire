985c1a711a61eda6383032d11e38783eb97fcba7
David Beauchamp <dbeauchamp@makermedia.com>
2015-11-13 18:40:48 -0800

    Pop-up store page template.

---
+++ wp-content/themes/makerfaire/page-make-store.php
@@ -0,0 +1,121 @@
+<?php
+/**
+* Template Name: Make Store Landing Page
+*/
+get_header(); ?>
+
+<!-- Header -->
+<header class="store-header">
+  <div class="container">
+    <div class="store-intro-text1">
+      <h3>Merry Making!</h3>
+      <h1><strong>Make:</strong> Store<br />Now open in Union Square, SF</h1>
+    </div>
+    <div class="store-intro-text2">
+      <h2><strong>345 Sutter St., b/t Grant + Stockton</strong><br />Open from November 25 through January 15, 2016</h2>
+      <h2>Open daily, 10am to 7pm</h2>
+      <p>(Closed Thanksgiving, Christmas, and New Year's Day)
+    </div>
+    <i class="fa fa-chevron-down"></i>
+  </div>
+</header>
+
+<!-- Apply blue bar -->
+<section class="store-apply-bar">
+  <p>WANT TO WORK AT THE STORE?  <a href="#">APPLY HERE</a>
+</section>
+
+<div class="container store-container">
+
+  <!-- Centered Stuff -->
+  <section class="store-centered-stuff">
+    <img class="img-responsive" src="/wp-content/themes/makerfaire/images/Make-store-makey_badge.jpg" alt="Makey Badge" />
+    <h2>Shop Make: In Real Life <span>#makestore</span></h2>
+    <p class="lead">Get the full #makestore experience with one of our special in-store events.</p>
+    <div class="row">
+      <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1">
+        <img class="img-responsive" src="/wp-content/themes/makerfaire/images/Make-store-kids.jpg" alt="Make Store kids at play" />
+      </div>
+    </div>
+    <p class="lead padtop">Stay tuned for our schedule of free DIY events for the whole family</p>
+  </section>
+
+  <!-- Photo Grid Section -->
+  <section class="store-pictures">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h2 class="section-heading">Portfolio</h2>
+          <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4 col-sm-6 portfolio-item">
+          <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img src="img/portfolio/roundicons.png" class="img-responsive" alt="">
+          </a>
+        </div>
+        <div class="col-md-4 col-sm-6 portfolio-item">
+          <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img src="img/portfolio/startup-framework.png" class="img-responsive" alt="">
+          </a>
+        </div>
+        <div class="col-md-4 col-sm-6 portfolio-item">
+          <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img src="img/portfolio/treehouse.png" class="img-responsive" alt="">
+          </a>
+        </div>
+        <div class="col-md-4 col-sm-6 portfolio-item">
+          <a href="#portfolioModal4" class="portfolio-link" data-toggle="modal">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img src="img/portfolio/golden.png" class="img-responsive" alt="">
+          </a>
+        </div>
+        <div class="col-md-4 col-sm-6 portfolio-item">
+          <a href="#portfolioModal5" class="portfolio-link" data-toggle="modal">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img src="img/portfolio/escape.png" class="img-responsive" alt="">
+          </a>
+        </div>
+        <div class="col-md-4 col-sm-6 portfolio-item">
+          <a href="#portfolioModal6" class="portfolio-link" data-toggle="modal">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img src="img/portfolio/dreams.png" class="img-responsive" alt="">
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <p class="store-red-footer lead text-center">PREFER TO SHOP ONLINE? FIND MORE PRESENTS AT <strong><a href="//www.makershed.com" target="_blank">MAKER SHED</a></strong></p>
+
+</div>
+
+<?php get_footer(); ?>

87885ce9171c1351fd5466c94915db9394383207
Alicia Williams <alicia.williams130@gmail.com>
2015-11-17 17:30:28 -0700

    change to return to the try list after updating an image

--- wp-content/themes/makerfaire/functions.php
+++ wp-content/themes/makerfaire/functions.php
@@ -1929,3 +1929,30 @@ function set_gv_formID(){
     
 }
 /*End changes to gravity view*/
+
+
+/**
+ * Change the update entry success message, including the link
+ * 
+ * @param $message string The message itself
+ * @param $view_id int View ID
+ * @param $entry array The Gravity Forms entry object
+ * @param $back_link string Url to return to the original entry
+ */
+function gv_my_update_message( $message, $view_id, $entry, $back_link ) {
+    $link = str_replace( 'entry/'.$entry['id'].'/', '', $back_link );
+    return 'Entry Updated. <a href="'.esc_url($link).'">Return to the list</a>';
+}
+add_filter( 'gravityview/edit_entry/success', 'gv_my_update_message', 10, 4 );
+
+/**
+ * Customise the cancel button link
+ *
+ * @param $back_link string
+ *
+ * since 1.11.1
+ */
+function gv_my_edit_cancel_link( $back_link, $form, $entry, $view_id ) {
+    return str_replace( 'entry/'.$entry['id'].'/', '', $back_link );
+}
+add_filter( 'gravityview/edit_entry/cancel_link', 'gv_my_edit_cancel_link', 10, 4 );

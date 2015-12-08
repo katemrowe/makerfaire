d664fbb9a3e0eb677841c0e097a1f54d5eb697eb
Rich Haynie <rhaynie@slashdotmedia.com>
2015-12-08 11:46:38 -0700

    Login fix.

--- wp-content/themes/makerfaire/classes/mf-login.php
+++ wp-content/themes/makerfaire/classes/mf-login.php
@@ -1,30 +1,107 @@
 <?php
 
+/**
+ * Custom Login Page Actions
+ */
+// Change the login url sitewide to the custom login page
+add_filter( 'login_url', 'custom_login_url', 10, 2 );
+// Redirects wp-login to custom login with some custom error query vars when needed
+add_action( 'login_head', 'custom_redirect_login', 10, 2 );
+// Updates login failed to send user back to the custom form with a query var
+add_action( 'wp_login_failed', 'custom_login_failed', 10, 2 );
+// Updates authentication to return an error when one field or both are blank
+add_filter( 'authenticate', 'custom_authenticate_username_password', 30, 3);
+// Automatically adds the login form to "login" page
+add_filter( 'the_content', 'custom_login_form_to_login_page' );
 
-/* Redirects to new login page */
+/**
+ * Custom Login Page Functions
+ */
+function custom_login_url( $login_url='', $redirect='' )
+{
+    $page = get_page_by_path('login');
+    if ( $page )
+    {
+        $login_url = get_permalink($page->ID);
 
-function redirect_login_page() {
-    $login_page  = home_url( '/login/' );
-    $page_viewed = basename($_SERVER['REQUEST_URI']);
- 
-    if( $page_viewed == "wp-login.php" && $_SERVER['REQUEST_METHOD'] == 'GET') {
-        wp_redirect($login_page);
+        if (! empty($redirect) )
+            $login_url = add_query_arg('redirect_to', urlencode($redirect), $login_url);
+    }
+    return $login_url;
+}
+function custom_redirect_login( $redirect_to='', $request='' )
+{
+    if ( 'wp-login.php' == $GLOBALS['pagenow'] )
+    {
+        $redirect_url = custom_login_url();
+
+        if (! empty($_GET['action']) )
+        {
+            if ( 'lostpassword' == $_GET['action'] )
+            {
+                return;
+            }
+            elseif ( 'register' == $_GET['action'] )
+            {
+                $register_page = get_page_by_path('register');
+                $redirect_url = get_permalink($register_page->ID);
+            }
+        }
+        elseif (! empty($_GET['loggedout'])  )
+        {
+            $redirect_url = add_query_arg('action', 'loggedout', custom_login_url());
+        }
+
+        wp_redirect( $redirect_url );
         exit;
     }
 }
-add_action('init','redirect_login_page');
-function login_failed() {
-    $login_page  = home_url( '/login/' );
-    wp_redirect( $login_page . '?login=failed' );
-    exit;
-}
-add_action( 'wp_login_failed', 'login_failed' );
- 
-function verify_username_password( $user, $username, $password ) {
-    $login_page  = home_url( '/login/' );
-    if( $username == "" || $password == "" ) {
-        wp_redirect( $login_page . "?login=empty" );
+function custom_login_failed( $username )
+{
+    $referrer = wp_get_referer();
+
+    if ( $referrer && ! strstr($referrer, 'wp-login') && ! strstr($referrer, 'wp-admin') )
+    {
+        if ( empty($_GET['loggedout']) )
+        wp_redirect( add_query_arg('action', 'failed', custom_login_url()) );
+        else
+        wp_redirect( add_query_arg('action', 'loggedout', custom_login_url()) );
         exit;
     }
 }
-add_filter( 'authenticate', 'verify_username_password', 1, 3);
+function custom_authenticate_username_password( $user, $username, $password )
+{
+    if ( is_a($user, 'WP_User') ) { return $user; }
+
+    if ( empty($username) || empty($password) )
+    {
+        $error = new WP_Error();
+        $user  = new WP_Error('authentication_failed', __('<strong>ERROR</strong>: Invalid username or incorrect password.'));
+
+        return $error;
+    }
+}
+function custom_login_form_to_login_page( $content )
+{
+    if ( is_page('login') && in_the_loop() )
+    {
+        $output = $message = "";
+        if (! empty($_GET['action']) )
+        {
+            if ( 'failed' == $_GET['action'] )
+                $message = "There was a problem with your username or password.";
+            elseif ( 'loggedout' == $_GET['action'] )
+                $message = "You are now logged out.";
+            elseif ( 'recovered' == $_GET['action'] )
+                $message = "Check your e-mail for the confirmation link.";
+        }
+
+        if ( $message ) $output .= '<div class="message"><p>'. $message .'</p></div>';
+        $output .= wp_login_form('echo=0&redirect='. site_url());
+        $output .= '<a href="'. wp_lostpassword_url( add_query_arg('action', 'recovered', get_permalink()) ) .'" title="Recover Lost Password">Lost Password?</a>';
+
+        $content .= $output;
+    }
+    
+    return $content;
+}
